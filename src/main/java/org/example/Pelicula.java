afb5053e0b17445b44f334845a2d9b40b289210e
Sofia <sofia.rios@qaracter.com>
2025-12-11 10:13:37 +0000

    Añadida catergoria a peliculas

--- src/main/java/org/example/Pelicula.java
+++ src/main/java/org/example/Pelicula.java
@@ -4,10 +4,12 @@ public class Pelicula {
     String name;
     String description;
     double rate;
+    String category;
 
-    public Pelicula(String name, double rate, String description){
+    public Pelicula(String name, double rate, String category, String description){
         this.name = name;
         this.rate = rate;
+        this.category = category;
         this.description = description;
     }
 
