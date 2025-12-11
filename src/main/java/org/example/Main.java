1e08437fcec93bc5540b85b783caeea6cd07ddd0
Sofia <sofia.rios@qaracter.com>
2025-12-11 09:41:12 +0000

    Creado metodo para filtrar pelis

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,11 @@ public class Main {
     public static void main(String[] args) {
         List<Pelicula> peliculas = new ArrayList<>();
         Pelicula peli1 = new Pelicula("Avatar", 7.7, "");
+
+        peliculas.add(new Pelicula("loque sea", 6.6, ""));
+    }
+
+    public List<Pelicula> filtrarPorRate(List<Pelicula> pelis, double rate){
+        return pelis;
     }
 }
