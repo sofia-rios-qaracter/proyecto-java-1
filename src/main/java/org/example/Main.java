e4376141bf6ef4bd47e72e99aa3c24fe9b990644
Francisco Valles <francisco.valles@qaracter.com>
2025-12-11 11:02:15 +0100

    Filtrado hecho

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,17 @@ public class Main {
         peliculas.add(new Pelicula("loque sea", 6.6, ""));
     }
 
-    public List<Pelicula> filtrarPorRate(List<Pelicula> pelis, double rate){
-        return pelis;
+    public List<Pelicula> filtrarPorRate(List<Pelicula> pelis, double rate) {
+        List<Pelicula> resultado = new ArrayList<>();
+
+        for (Pelicula p : pelis) {
+            if (p.rate >= rate) {   // ACCESO DIRECTO AL ATRIBUTO
+                resultado.add(p);
+            }
+        }
+
+        return resultado;
     }
+
+
 }
